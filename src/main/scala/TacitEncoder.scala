33cd9825104d5b1f1f9c7491232dc6aab05aed8f
anshmaroo <amaroo2006@gmail.com>
2025-09-22 15:46:53 -0700

    fix: queue syntax

--- src/main/scala/TacitEncoder.scala
+++ src/main/scala/TacitEncoder.scala
@@ -222,17 +222,12 @@ class TacitEncoderModule(outer: TacitEncoder) extends LazyTraceEncoderModule(out
   pipeline_advance := io.in.group.map(g => g.iretire === 1.U).reduce(_ || _) // check for a valid instruction packet
 
   when (pipeline_advance) {    
-    // Make sure this is correct - check for any messages in the input packet - if no messages, reset ingress 0
-    when (io.in.group.map(g => g.itype =/= TraceItype.ITNothing && g.iretire === 1.U).reduce(_ || _)) {
-      ingress_0 := io.in
-    } .otherwise {
-      ingress_0 := 0.U
-    }
+    ingress_0 := io.in
     
     ingress_1 := ingress_0 // to be deleted
 
-    // only enqueue entries when they actually exist
-    when (ingress_0.group.map(g=> g.iretire === 1.U).reduce(_ || _)) {
+    // only enqueue entries when they actually exist and have some information 
+    when (ingress_0.group.map(g=> g.iretire === 1.U && g.itype =/= TraceItype.ITNothing).reduce(_ || _)) {
       ingress_1_queue.io.enq.valid := true.B
       ingress_1_queue.io.enq.bits := ingress_0
     }
@@ -356,12 +351,12 @@ class TacitEncoderModule(outer: TacitEncoder) extends LazyTraceEncoderModule(out
   
 
   // at this point there should only be valid (message-containing) entries in ingress 1 queue
-  val ingress_1_has_message = ingress_1_queue.deq.bits.group.map(g => g.itype =/= TraceItype.ITNothing && g.iretire === 1.U).reduce(_ || _)
-  val ingress_1_has_branch = ingress_1_queue.deq.bits.group.map(g => (g.itype === TraceItype.ITBrTaken || g.itype === TraceItype.ITBrNTaken) && g.iretire === 1.U).reduce(_ || _)
+  val ingress_1_has_message = ingress_1_queue.io.deq.bits.group.map(g => g.itype =/= TraceItype.ITNothing && g.iretire === 1.U).reduce(_ || _)
+  val ingress_1_has_branch = ingress_1_queue.io.deq.bits.group.map(g => (g.itype === TraceItype.ITBrTaken || g.itype === TraceItype.ITBrNTaken) && g.iretire === 1.U).reduce(_ || _)
   val ingress_1_has_packet = Mux(is_bp_mode, ingress_1_has_message && !ingress_1_has_branch, ingress_1_has_message)
-  val ingress_1_msg_idx = PriorityEncoder(ingress_1_queue.deq.bits.group.map(g => g.itype =/= TraceItype.ITNothing && g.iretire === 1.U))
+  val ingress_1_msg_idx = PriorityEncoder(ingress_1_queue.io.deq.bits.group.map(g => g.itype =/= TraceItype.ITNothing && g.iretire === 1.U))
 
-  val ingress_1_valid_count = PopCount(ingress_1_queue.deq.bits.group.map(g => g.iretire === 1.U))
+  val ingress_1_valid_count = PopCount(ingress_1_queue.io.deq.bits.group.map(g => g.iretire === 1.U))
 
   // this needs to be adjusted to not only read within a group but within the queue
   /************* FIX ME ***************/ 
